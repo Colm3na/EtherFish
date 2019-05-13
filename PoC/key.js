626f6196ba2b5dbf4c9e8d4451f11f63a7acfd2f
mike <magma1983@gmail.com>
2019-05-13 23:04:25 +0200

    .gitignore

--- PoC/key.js
+++ PoC/key.js
@@ -1,1 +1,1 @@
-web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/INFURA-KEY"));
+web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/97931dc0f4dd46898a20b2d6fa0ee3ab"));
