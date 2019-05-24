e1c0f79885eec7215d1f30d1d8191753e9098f9d
mike <magma1983@gmail.com>
2019-05-24 22:41:20 +0200

    main.js

--- src/main.js
+++ src/main.js
@@ -1,52 +1,21 @@
 const Web3 = require('web3');
 const fs = require('fs');
+const Motor = require('./motor.js'); // Module GPIO (Raspberry Pi 2)
+
 var infuraURL = fs.readFileSync('../secret/infuraURL.txt', 'utf8');
-const web3 = new Web3(new Web3.providers.HttpProvider(infuraURL));
-const Tx = require('ethereumjs-tx');
+const web3 = new Web3(new Web3.providers.WebsocketProvider(infuraURL));
 
-// Module GPIO (Raspberry Pi 2)
-const Motor = require('./motor.js');
-
-var privateKey0 = fs.readFileSync('../secret/privateKey.txt', 'utf8');
-const privateKey = new Buffer(privateKey0, 'hex');
-const pk = '0x' + privateKey0;
-var _value = web3.utils.toHex(web3.utils.toWei('0.0001', 'ether'));
-var _nonce;
-
-var address_obj= web3.eth.accounts.privateKeyToAccount(pk);
-const _from = address_obj["address"];
-const _to = "0xdf588d9726305d68663e6f025b2c12db7042208e";
-var rawTx = {
-	"nonce": _nonce,
-	"gasPrice": 41000000000,
-	"gas": 21000,
-	"to": _to,
-	"value": _value,
-	"data": "0x",
-	"chainId": 4
+var options = {
+	fromBlock: 4439954,
+	address: "0x0451a1c0E4194967363aD9cF1A202419Ba4fe83E",
+	topics: ["0xd7912fcf88b9813d94870cbde9f39a918aa37a93e03cb9ffab574870931eb56d"]
 };
-var signedTx;
 
-function CB_getTxCount(err,res) {
-	if(!err) {_nonce = res;}
-}
-
-function CB_signTx(err,res) {
-	if(!err) {
-		signedTx = res["rawTransaction"];
-		console.log(signedTx);
-		const tx = new Tx(rawTx);
-		tx.sign(privateKey);
-		const serializedTx = tx.serialize();
-		web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('transactionHash', (res) => {
-			console.log(res);
-			Motor.rotationFW();
-			setTimeout(Motor.rotationBW, 2000);
-			setTimeout(Motor.endRotation, 4000);
-		});
-	}
-}
-
-web3.eth.getTransactionCount(_from,"latest",CB_getTxCount)
-	.then(web3.eth.accounts.signTransaction(rawTx,pk,CB_signTx));
+var subscription = web3.eth.subscribe('logs',options);
+subscription.on('data', (log) => {
+	console.log(log);
+	Motor.rotationFW();
+	setTimeout(Motor.rotationBW, 2000);
+	setTimeout(Motor.endRotation, 4000);
+});
 
